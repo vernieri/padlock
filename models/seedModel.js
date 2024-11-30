86fd38302c235b12ec30b474901c8d046bd5f32a
jvernieri <jvmendes.ti@gmail.com>
2024-11-30 12:37:31 -0300

    Searching for seedID

--- models/seedModel.js
+++ models/seedModel.js
@@ -1,9 +1,15 @@
 const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid");
 
 const seedSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  seed: { type: String, required: true, unique: true },
+  seed: {
+    type: String,
+    required: true,
+    unique: true,
+    default: uuidv4, // Gera um UUID automaticamente
+  },
 });
 
 const Seed = mongoose.model("Seed", seedSchema);
