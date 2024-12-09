3e0e8f610ef30e11f3e83570b37c056cda28139e
Vernieri <jvmendes.ti@gmail.com>
2024-12-09 14:06:39 -0300

    adding more tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   testEnvironment: "node",
-  testTimeout: 30000, // Ajuste para lidar com operações assíncronas mais longas
+  testTimeout: 30000, // Ajuste para testes assíncronos mais longos
 };
---
+++ test/auth.test.js
@@ -0,0 +1,40 @@
+const request = require("supertest");
+const app = require("../app"); // Certifique-se de exportar o app no app.js
+const { connectDB, disconnectDB } = require("./setupTestDB");
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await disconnectDB();
+});
+
+describe("Auth API", () => {
+  test("Deve registrar um novo usuário", async () => {
+    const res = await request(app).post("/api/auth/register").send({
+      username: "testuser",
+      email: "test@example.com",
+      password: "password123",
+    });
+
+    expect(res.statusCode).toBe(201);
+    expect(res.body.token).toBeDefined();
+  });
+
+  test("Deve autenticar um usuário e retornar um token JWT", async () => {
+    await request(app).post("/api/auth/register").send({
+      username: "testuser",
+      email: "test@example.com",
+      password: "password123",
+    });
+
+    const res = await request(app).post("/api/auth/login").send({
+      email: "test@example.com",
+      password: "password123",
+    });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.token).toBeDefined();
+  });
+});
